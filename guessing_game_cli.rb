1c8f099ad9ac7ac5430cf6c8c08237c8a3a249b2
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-30 11:24:54 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,17 +1,17 @@
 # def run_guessing_game
-#   random_num = rand(1..6)
 #   input = gets.chomp
 #
 #   while input != "exit" do
-#     if input == random_num.to_s
-#       print "You guessed the correct number!"
+#   random_num = rand(1..6)
+#     if input.to_i == random_num
+#       puts "You guessed the correct number!"
 #     else
-#       print "Sorry! The computer guessed #{random_num}."
+#       puts "Sorry! The computer guessed #{random_num}."
 #     end
 #     random_num = rand(1..6)
 #     input = gets.chomp
 #   end
-#   print "Goodbye"
+#   puts "Goodbye"
 # end
 
 def run_guessing_game()
